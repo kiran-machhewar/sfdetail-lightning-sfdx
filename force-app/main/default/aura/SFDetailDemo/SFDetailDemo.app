e756c64d9dc00f76f54074f3c1e77aa8f0dc5841
Kiran Machhewar <smachhewar@gmail.com>
2017-10-01 17:47:51 -0700

    updated sfdetail component to show fetched details on page

---
+++ force-app/main/default/aura/SFDetailDemo/SFDetailDemo.app
@@ -0,0 +1,5 @@
+<aura:application extends="force:slds">
+    <c:sfdetail sobjectId='a001F000000xikS' 
+        fieldsets='section1;section2' 
+        sectionNames='section1title;section2title' ></c:sfdetail>        
+</aura:application>	
--- force-app/main/default/aura/sfdetail/sfdetailController.js
+++ force-app/main/default/aura/sfdetail/sfdetailController.js
@@ -1,5 +1,30 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+    doInit : function(component, event, helper) {
+		// Create the action
+        var action = component.get("c.initialize");
+		action.setParams({
+            sobjectId           : component.get("v.sobjectId"),
+            fieldsets           : component.get("v.fieldsets"),
+            sectionNames        : component.get("v.sectionNames")           
+		});
+		component.set("v.showSpinner","true");
+		// Add callback behavior for when response is received
+		action.setCallback(this, function(response) {
+		    component.set("v.showSpinner","false");
+		    var state = response.getState();  
+		    if (component.isValid() && state === "SUCCESS" ) {
+                var result = response.getReturnValue();
+                component.set("v.data",result);
+                console.log(result);
+							
+		    }
+		    else {
+                console.log("Failed with state: " + state);
+               			
+            }            
+		    
+		});
+		// Send action off to be executed
+		$A.enqueueAction(action);
+	}
 })
