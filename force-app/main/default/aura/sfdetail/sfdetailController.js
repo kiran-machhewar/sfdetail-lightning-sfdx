c084a45b69a7f7278361e458ba4f9a5666a22f4f
Kiran Machhewar <smachhewar@gmail.com>
2017-10-07 13:06:37 -0700

    closes  #3, closes #4, closes #8

--- force-app/main/default/aura/sfdetail/sfdetailController.js
+++ force-app/main/default/aura/sfdetail/sfdetailController.js
@@ -5,10 +5,13 @@
 		action.setParams({
             sobjectId           : component.get("v.sobjectId"),
             fieldsets           : component.get("v.fieldsets"),
-            sectionNames        : component.get("v.sectionNames")           
+			sectionNames        : component.get("v.sectionNames"),
+			columnSizeStr		: ''+component.get("v.columnSize")           
 		});
 		component.set("v.showSpinner","true");
 		// Add callback behavior for when response is received
+
+		var toastEvent = $A.get("e.force:showToast");
 		action.setCallback(this, function(response) {
 		    component.set("v.showSpinner","false");
 		    var state = response.getState();  
@@ -18,8 +21,14 @@
                 console.log(result);							
 		    }
 		    else {
-                console.log("Failed with state: " + state);
-               			
+				console.log("Failed with state: " + state);
+				console.log("response",response);	
+				toastEvent.setParams({
+					"title": "Error!",
+					"type": "error",
+					"message": "Error "+response.getError()[0].message
+				});	
+				toastEvent.fire();
             }            
 		    
 		});
